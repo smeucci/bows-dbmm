cce7dd20690f9545e58e52fcd8e2528354c9a052
Lorenzo Cioni <lore.cioni@gmail.com>
2015-11-04 09:41:14 +0100

    k nearest neighbours function

--- kNearestNeighbours.m
+++ kNearestNeighbours.m
@@ -2,10 +2,13 @@ function dmatk = kNearestNeighbours(dmat, K)
 % Return matrix of the first k-nearest neighbours for each row 
 % of a distance matrix
     
+    % Sort distance matrix by each row
+    dmatk = sort(dmat, 2);
     
-    
-    dmatk = dmat;
-
+    if K < (size(dmatk, 2) - 1)
+        % Save only K-nearest neighbours, set others to 0
+        dmatk(:, K+1:end) = 0;
+    end
 
 end
 
